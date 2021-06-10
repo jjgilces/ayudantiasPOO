389f2d984ff2c16d6a85091b75bd5fdef6ca6bee
jjgilces <jjgilces@espol.edu.ec>
2021-06-10 14:17:52 -0500

    Cree nuevos atributos como el contador

--- Proyecto1P/src/proyecto1p/Proyecto1P.java
+++ Proyecto1P/src/proyecto1p/Proyecto1P.java
@@ -15,7 +15,7 @@ public class Proyecto1P {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola");
     }
     
 }
